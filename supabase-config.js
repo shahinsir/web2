2cd1123dffddf23f92fa07b09ae3eb8409760141
shahinsir <alamshahinur242@gmail.com>
2026-01-30 23:50:44 +0600

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,7 @@
 // ====================================================
 
 const SUPABASE_URL = "https://cczayjqesmfimhedarzt.supabase.co";  // আপনার Supabase Project URL
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjemF5anFlc21maW1oZWRhcnp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2OTQ5NjQsImV4cCI6MjA4NTI3MDk2NH0.SsiTxRliy5oJxjH47bW6rukX367_iGVCSZpZF3-1CC4";                 // আপনার anon public key
+const SUPABASE_KEY = "sb_publishable_uJgBtz07LsFp_6R3yD5l3w_oFRYe9p7";                 // আপনার anon public key
 
 // Supabase client
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
